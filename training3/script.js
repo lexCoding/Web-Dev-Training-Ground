e612d94cb5b2c7c1fe04c3ad865656385eb6c58c
lexCoding <lexCoding@gmail.com>
2022-04-05 04:04:43 +0800

    Finished freeCodeCamp Responsive #2 with validation

--- training3/script.js
+++ training3/script.js
@@ -1,14 +1,26 @@
 'use-strict';
 
-const dropdown = document.querySelector('.dropdown')
+const dropdown = document.querySelector('.role')
+const dropdownFeature = document.querySelector('.feature')
+const dropdownFeature2 = document.querySelector('.feature-2')
+const dropdownFeature3 = document.querySelector('.feature-3')
 const dropdownList = document.querySelector('.dropdown-list')
 const select = document.querySelector('.select')
+
 
 dropdown.addEventListener('click', function(e){
     dropdownList.classList.toggle('show')
     const data = e.target.closest('li')
     
     if(!data) return;
-    select.textContent = data.dataset.choice
+    select.textContent = data.dataset.choice;
+})
+
+dropdownFeature.addEventListener('click', function(e){
     
+    dropdownFeature3.classList.toggle('show')
+    const data = e.target.closest('li')
+    console.log(data)
+    if(!data) return;
+    dropdownFeature2.textContent = data.dataset.choice;
 })
